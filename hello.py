b672be8eba672402106b95a99eb595eb3e6c2f31
Sergio Tarquini <stitpro@gmail.com>
2018-02-02 13:00:35 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,10 +4,7 @@ import click
 @click.argument('name')
 
 def hello():
-    """
-    Example script
-    see: http://click.pocoo.org/5/setuptools/
-    """
+
     click.echo('Hello '+name+'!')
 
 if __name__ == '__main__':
