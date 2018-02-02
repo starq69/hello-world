5e1555401f8d85ad60c57385afeafb291dd51d08
starq69 <starq@minione.com>
2018-02-02 12:24:16 +0100

    patched

--- hello.py
+++ hello.py
@@ -2,9 +2,13 @@ import click
 
 @click.command()
 @click.argument('name')
-def cli():
+
+def hello():
     """
     Example script
     see: http://click.pocoo.org/5/setuptools/
     """
     click.echo('Hello '+name+'!')
+
+if __name__ == '__main__':
+	hello()
